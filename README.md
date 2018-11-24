d1c7a4447bcbc1d58d1b45a08e743636e1d1bc03
Nikas Praninskas <nikaspran@gmail.com>
2018-11-24 21:01:03 +0200

    Generate bootstrap confirmation blocks on setup

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # bitcoin-testbox
 
-A super simple regtest client for Bitcoin
+A super simple regtest client for Bitcoin to run integration tests
+
+## Setup
+
+```
+npm install --save-dev bitcoin-testbox
+// or
+yarn add --dev bitcoin-testbox
+```
+
+## Usage
+
+## Troubleshooting
+
+```
+npm rebuild bitcoin-testbox
+// or
+yarn add bitcoin-testbox --force
+```
+
+
+
--- src/setup.js
+++ src/setup.js
@@ -1,5 +1,37 @@
 /* eslint-disable no-console */
+const http = require('http');
 const { docker, untilFinished, getImageName } = require('./utils/dockerUtils');
+const bitcoin = require('./index');
+
+async function generateBootstrapConfirmations({
+  host, port, username, password,
+}) {
+  return new Promise((resolve, reject) => {
+    const data = '{"jsonrpc":"1.0","id":"1","method":"generate","params":[100]}';
+
+    const req = http.request({
+      hostname: host,
+      auth: `${username}:${password}`,
+      port,
+      path: '/',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(data),
+      },
+    }, (res) => {
+      if (res.statusCode === 200) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+
+    req.on('error', reject);
+    req.write(data);
+    req.end();
+  });
+}
 
 async function run() {
   const imageName = await getImageName({ create: true });
@@ -11,6 +43,10 @@ async function run() {
 
   const logs = await untilFinished(stream);
   console.log(logs);
+
+  const credentials = await bitcoin.start();
+  await generateBootstrapConfirmations(credentials);
+  await bitcoin.stop();
 }
 
 run()
